7e13c4f010d801101c7b644dd89db90d3f0205fd
Luis Rojas <luis.andres81@gmail.com>
2019-01-15 00:04:05 +0100

    Added scriptable variables

---
+++ Scripts/ScriptableGameEvent.cs
@@ -0,0 +1,34 @@
+﻿namespace HalfBlind.ScriptableVariables {
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "OnGameEvent", menuName = "GameEvents/ScriptableEvent")]
+    public class ScriptableGameEvent : ScriptableObject {
+        private HashSet<Action> _callbacks = new HashSet<Action>();
+
+        private void OnEnable() {
+            _callbacks.Clear();
+        }
+
+        private void OnDisable() {
+            _callbacks.Clear();
+        }
+
+        [Sirenix.OdinInspector.HideInEditorMode]
+        //[Sirenix.OdinInspector.Button(Sirenix.OdinInspector.ButtonSizes.Gigantic)]
+        public virtual void SendEvent() {
+            foreach (var callback in _callbacks) {
+                callback();
+            }
+        }
+
+        public void AddListener(Action callback) {
+            _callbacks.Add(callback);
+        }
+
+        public void RemoveListener(Action callback) {
+            _callbacks.Remove(callback);
+        }
+    }
+}
