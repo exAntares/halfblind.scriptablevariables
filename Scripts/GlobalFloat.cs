3883c4a26b951188bb120f948b6390b1a2aaa04b
Luis Rojas <luis.andres81@gmail.com>
2019-04-19 10:44:34 +0200

    Small editor to see values on global float and global list float

--- Scripts/GlobalFloat.cs
+++ Scripts/GlobalFloat.cs
@@ -1,6 +1,17 @@
 ﻿namespace HalfBlind.ScriptableVariables {
     using UnityEngine;
 
+#if UNITY_EDITOR
+    [UnityEditor.CustomEditor(typeof(GlobalFloat))]
+    public class GlobalFloatEditor: UnityEditor.Editor {
+
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+            UnityEditor.EditorGUILayout.LabelField($"CurrentValue : {(target as GlobalFloat).Value}");
+        }
+    }
+#endif
+
     [CreateAssetMenu(fileName = "MyFloat", menuName = "Variables/Float")]
     public class GlobalFloat : ScriptableVariable<float> {
         public override void FromString(string value) {
--- Scripts/GlobalListFloat.cs
+++ Scripts/GlobalListFloat.cs
@@ -2,6 +2,20 @@
 using UnityEngine;
 
 namespace HalfBlind.ScriptableVariables {
+#if UNITY_EDITOR
+    [UnityEditor.CustomEditor(typeof(GlobalListFloat))]
+    public class GlobalListFloatEditor : UnityEditor.Editor {
+
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+            var list = (target as GlobalListFloat).Value;
+            for (int i = 0; i < list.Count; i++) {
+                UnityEditor.EditorGUILayout.LabelField($"Item{i:00}: {list[i]}");
+            }
+        }
+    }
+#endif
+
     [CreateAssetMenu(fileName = "GlobalListFloat", menuName = "Variables/List/Float")]
     public class GlobalListFloat : ScriptableVariable<List<float>> {
         [SerializeField] private string _separator = ";";
