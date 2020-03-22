09e12a0d9116623aab73cc98ba5eb008a4fa8cd5
Luis Rojas <luis.andres81@gmail.com>
2020-03-22 17:24:43 +0100

    New Global Game Object + setter

---
+++ Scripts/GlobalGameObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace HalfBlind.ScriptableVariables {
+	[CreateAssetMenu(fileName = nameof(GlobalGameObject), menuName = "Variables/"+nameof(GlobalGameObject))]
+	public class GlobalGameObject : ScriptableVariable<GameObject> {
+		public override void FromString(string value) {
+			Value = GameObject.Find(value);
+		}
+	}
+}
---
+++ Scripts/GlobalGameObjectSetter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace HalfBlind.ScriptableVariables {
+	public class GlobalGameObjectSetter : MonoBehaviour {
+		[SerializeField] private GlobalGameObject _globalGameObject;
+		public void Awake() {
+			_globalGameObject.Value = gameObject;
+		}
+	}
+}
