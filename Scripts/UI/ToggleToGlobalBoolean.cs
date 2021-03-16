95158dd41cacd955fac0f0e901a9f7b6576d9994
Luis Rojas <luis.andres81@gmail.com>
2021-03-16 21:28:18 +0100

    Sync global booleans with ui toggles

---
+++ Scripts/UI/ToggleToGlobalBoolean.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace HalfBlind.ScriptableVariables {
+    public class ToggleToGlobalBoolean : MonoBehaviour {
+        [SerializeField] private Toggle _toggle;
+        [SerializeField] private GlobalBoolean _globalBoolean;
+        
+        private void Awake() {
+            _toggle.isOn = _globalBoolean.Value;
+            _toggle.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        private void OnDestroy() => _toggle.onValueChanged.RemoveListener(OnValueChanged);
+
+        private void Reset() => _toggle = GetComponent<Toggle>();
+
+        private void OnValueChanged(bool isOn) {
+            if (_globalBoolean.Value != isOn) {
+                _globalBoolean.Value = isOn;
+            }
+        }
+    }
+}
